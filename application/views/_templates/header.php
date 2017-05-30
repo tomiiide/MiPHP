2d21571401330a79e8554f4019a91fe093d6b320
tomiiide <ayotomiiide@gmail.com>
2017-05-30 15:45:58 +0100

    Login view done

--- application/views/_templates/header.php
+++ application/views/_templates/header.php
@@ -32,6 +32,9 @@
              <li class="nav-item">
                  <a class="nav-link" href="<?=URL;?>home/register">Register</a>
              </li>
+             <li class="nav-item">
+                 <a class="nav-link" href="<?=URL;?>admin/login">Login</a>
+             </li>
          </ul>
      </div>
  </nav>
--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -1,6 +1,28 @@
-<form action="" method="POST">
-  <input type="text" placeholder="Username" name="username"><br><br>
-  <input type="password" placeholder="Password" name="password"><br><br>
-  <input type="submit" value="Login">
+<div class="container col-md-6 offset-md-3">
 
-</form>
+    <form action="" method="POST" class="form" role="form">
+        <div class="form-group">
+            <legend>MiPHP /Login</legend>
+        </div>
+        <?php if(isset($message)){ ?>
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <?= $message; ?>;
+            </div>
+        <?php } ?>
+
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="" placeholder="Input Email" name="email">
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" id="" placeholder="Input password" name="password">
+        </div>
+        <div class="form-group">
+            <div class="col-sm-10 col-sm-offset-2">
+                <button type="submit" class="btn btn-primary" name="login">Login</button>
+            </div>
+        </div>
+    </form>
+</div>
